609d06e95e66771f7f26ac32f9830f03efcb2e5d
delvis1205 <moraisdelvis75@gmail.com>
2025-09-18 14:27:46 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -5,21 +5,21 @@ export const products = [
     game: "Free Fire",
     title: "Diamantes 100",
     priceAOA: 1200,
-    image: "assets/ff_diamantes_100.png"
+    image: "ff_diamantes_100.png"
   },
   {
     id: "ff-310d",
     game: "Free Fire",
     title: "Diamantes 310",
     priceAOA: 3200,
-    image: "assets/ff_diamantes_310.png"
+    image: "ff_diamantes_310.png"
   },
   {
     id: "ff-pass",
     game: "Free Fire",
     title: "Passe de Elite (mensal)",
     priceAOA: 4500,
-    image: "assets/ff_passe_elite.png"
+    image: "ff_passe_elite.png"
   },
 
   // Delta Force M
@@ -28,14 +28,14 @@ export const products = [
     game: "Delta Force M",
     title: "Gold 500",
     priceAOA: 2500,
-    image: "assets/dfm_gold_500.png"
+    image: "dfm_gold_500.png"
   },
   {
     id: "dfm-gold1200",
     game: "Delta Force M",
     title: "Gold 1200",
     priceAOA: 5200,
-    image: "assets/dfm_gold_1200.png"
+    image: "dfm_gold_1200.png"
   },
 
   // eFootball
@@ -44,14 +44,14 @@ export const products = [
     game: "eFootball",
     title: "Moedas eFootball 250",
     priceAOA: 1900,
-    image: "assets/efc_moedas_250.png"
+    image: "efc_moedas_250.png"
   },
   {
     id: "efc-coin1050",
     game: "eFootball",
     title: "Moedas eFootball 1050",
     priceAOA: 6900,
-    image: "assets/efc_moedas_1050.png"
+    image: "efc_moedas_1050.png"
   },
 
   // Outros
@@ -60,14 +60,14 @@ export const products = [
     game: "Outros",
     title: "Gift Card Xbox 50",
     priceAOA: 15000,
-    image: "assets/gift_xbox_50.png"
+    image: "gift_xbox_50.png"
   },
   {
     id: "out-psn-gift50",
     game: "Outros",
     title: "Gift Card PSN 50",
     priceAOA: 17000,
-    image: "assets/gift_psn_50.png"
+    image: "gift_psn_50.png"
   },
   // Novos produtos
   {
@@ -75,55 +75,55 @@ export const products = [
     game: "Free Fire",
     title: "Diamantes 560",
     priceAOA: 5200,
-    image: "assets/ff_diamantes_310.png"
+    image: "ff_diamantes_310.png"
   },
   {
     id: "ff-1120d",
     game: "Free Fire",
     title: "Diamantes 1120",
     priceAOA: 9800,
-    image: "assets/ff_diamantes_310.png"
+    image: "ff_diamantes_310.png"
   },
   {
     id: "ff-pass3",
     game: "Free Fire",
     title: "Passe de Elite 3 meses",
     priceAOA: 12500,
-    image: "assets/ff_passe_elite.png"
+    image: "ff_passe_elite.png"
   },
   {
     id: "dfm-gold2500",
     game: "Delta Force M",
     title: "Gold 2500",
     priceAOA: 9800,
-    image: "assets/dfm_gold_1200.png"
+    image: "dfm_gold_1200.png"
   },
   {
     id: "dfm-gold750",
     game: "Delta Force M",
     title: "Gold 750",
     priceAOA: 3600,
-    image: "assets/dfm_gold_500.png"
+    image: "dfm_gold_500.png"
   },
   {
     id: "efc-coin2150",
     game: "eFootball",
     title: "Moedas eFootball 2150",
     priceAOA: 12500,
-    image: "assets/efc_moedas_1050.png"
+    image: "efc_moedas_1050.png"
   },
   {
     id: "efc-coin500",
     game: "eFootball",
     title: "Moedas eFootball 500",
     priceAOA: 3600,
-    image: "assets/efc_moedas_250.png"
+    image: "efc_moedas_250.png"
   },
   {
     id: "out-xbox-gift50-b",
     game: "Outros",
     title: "Gift Card Xbox 2x50 (100)",
     priceAOA: 29000,
-    image: "assets/gift_xbox_50.png"
+    image: "gift_xbox_50.png"
   }
 ];
